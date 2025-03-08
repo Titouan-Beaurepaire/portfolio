ec503622ab629ef47137912b810611a435ea5359
Titouan Beaurepaire <titouan-luigi-roland.beaurepaire@epitech.eu>
2025-03-08 02:02:18 +0100

    [Add][Header with name, need video for replace texte white color]

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,15 @@ import './App.css'
 
 function App() {
   return (
-    <div>
-      <h1>Bienvenue sur mon portfolio</h1>
+    <div className="background-image">
+      <video className="video-background" autoPlay loop muted>
+        <source src="./assets/lemans.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <header className="header">
+        <h2>Titouan</h2>
+        <h1>BEAUREPAIRE</h1>
+      </header>
     </div>
   )
 }
