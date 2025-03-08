8240440c25f33ed2949c9c59d84e71f335939824
Titouan Beaurepaire <titouan-luigi-roland.beaurepaire@epitech.eu>
2025-03-08 01:09:27 +0100

    [Add][first push]

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-//import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  return (
+    <div>
+      <h1>Bienvenue sur mon portfolio</h1>
+    </div>
+  )
 }
 
 export default App
